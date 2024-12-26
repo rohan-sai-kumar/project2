a27a33ca7bc9e27220aa11d534b521565635b20f
rohan <rohansaikumar71@gmail.com>
2024-12-26 10:59:13 +0530

    my commit

---
+++ travello/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from  .models import Destination,Detailed_desc,pessanger_detail,ContactUS
+# Register your models here.
+
+admin.site.register(Destination)
+admin.site.register(Detailed_desc)
+admin.site.register(pessanger_detail)
+admin.site.register(ContactUS)
+
